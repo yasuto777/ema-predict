d7498e482d858cdf9df80d7aec6b73309755da6b
yasuto777 <basilisk.0331@icloud.com>
2018-01-25 07:45:50 +0000

    Use DB in container

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'NAME': 'app',
-        #'HOST': 'db',
-        #'USER': 'root',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
